bd3af93ce466bb3e99420d1c5a8706a8c2f9fb02
Melvin Versluijs <melvin.versluijs@outlook.com>
2019-02-03 16:40:32 +0100

    Save list items to localstorage for some caching.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import List from "./components/List";
 import AddItem from "./components/AddItem";
 import "./App.css";
 
+//Local storage key for items.
+const LOCAL_STORAGE_ITEMS_KEY = "list_items";
+
 /**
  * Class App, starting component.
  */
 class App extends React.Component {
-  state = {
-    items: [
-      {
-        id: 1,
-        label: "TODO 1",
-        completed: true
-      }
-    ]
-  };
+  constructor() {
+    super();
+
+    const items = localStorage.getItem(LOCAL_STORAGE_ITEMS_KEY);
+    this.state = {
+      items: items ? JSON.parse(items) : []
+    };
+  }
 
   /**
    * Add item
@@ -23,16 +25,24 @@ class App extends React.Component {
    * @param label - label for the new list item.
    */
   addItem = label => {
-    this.setState(prev => ({
-      items: [
-        ...prev.items,
-        {
-          id: Math.ceil(Math.random() * Math.floor(99999)),
-          label: label,
-          completed: false
-        }
-      ]
-    }));
+    this.setState(
+      prev => ({
+        items: [
+          ...prev.items,
+          {
+            id: Math.ceil(Math.random() * Math.floor(99999)),
+            label: label,
+            completed: false
+          }
+        ]
+      }),
+      () => {
+        localStorage.setItem(
+          LOCAL_STORAGE_ITEMS_KEY,
+          JSON.stringify(this.state.items)
+        );
+      }
+    );
   };
 
   /**
@@ -41,9 +51,17 @@ class App extends React.Component {
    * @param id - id for the to be deleted item.
    */
   deleteItem = id => {
-    this.setState(prev => ({
-      items: prev.items.filter(item => id !== item.id)
-    }));
+    this.setState(
+      prev => ({
+        items: prev.items.filter(item => id !== item.id)
+      }),
+      () => {
+        localStorage.setItem(
+          LOCAL_STORAGE_ITEMS_KEY,
+          JSON.stringify(this.state.items)
+        );
+      }
+    );
   };
 
   /**
@@ -52,15 +70,23 @@ class App extends React.Component {
    * @param id - id for the updated item.
    */
   toggleCompleted = id => {
-    this.setState(prev => ({
-      items: prev.items.map(item => {
-        if (item.id === id) {
-          item.completed = !item.completed;
-        }
+    this.setState(
+      prev => ({
+        items: prev.items.map(item => {
+          if (item.id === id) {
+            item.completed = !item.completed;
+          }
 
-        return item;
-      })
-    }));
+          return item;
+        })
+      }),
+      () => {
+        localStorage.setItem(
+          LOCAL_STORAGE_ITEMS_KEY,
+          JSON.stringify(this.state.items)
+        );
+      }
+    );
   };
 
   render() {
